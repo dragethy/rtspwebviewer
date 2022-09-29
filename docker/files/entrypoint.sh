022d2a8bb0fda8ce6886cb75a7b395bcae12e905
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-09-30 00:44:35 +0100

    Removing unnecessary stuff in Dockerfile.

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Launch rtspwebviewer web server
-sudo -u $USER /bin/zsh -c "source $HOME/.zshrc && python -m rtspwebviewer.run -u $RWV_RTSP_ADDRESS -a $RWV_LISTENING_IP -p $RWV_PORT -t $RWV_TITLE"
-
 # Run the CMD specified by the user
 sh -c "$(echo $@)"

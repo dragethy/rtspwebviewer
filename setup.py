5c4ac5ec0dba10ef2d039b8a8423e0094faf4b0b
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-10-01 20:36:47 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setuptools.setup(name='rtspwebviewer',
-    version='0.1.1',
+    version='0.1.2',
     description='Simple web viewer for RTSP streams.',
     author='Luis C. Garcia-Peraza Herrera',
     author_email='luiscarlos.gph@gmail.com',
